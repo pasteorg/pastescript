36b7a963158d354f50ea0cc60894d9dd6dca1dca
ianb <ianb@localhost>
2006-11-26 23:37:03 +0000

    fix from pythy for the twisted.web2 wrapper, resolving (and from patch from) #138.  Not tested; hopefully the submitter did necessary testing (not surprising that it was previously broken)

--- paste/script/twisted_web2_server.py
+++ paste/script/twisted_web2_server.py
@@ -2,55 +2,19 @@
 # Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
 # @@: THIS IS INCOMPLETE!
 
-from paste.deploy.converters import asbool
-
-def conn_string(host, port, **params):
-    s = 'tcp:%s' % port
-    if host:
-        s += ':interface=%s' % host
-    for name, value in params.items():
-        s += ':%s=%s' % (name, value)
-    return s
-    
-
 def run_twisted(wsgi_app, global_conf,
-                use_http=True,
-                host='127.0.0.1', port='8080',
-                use_https=False,
-                https_host=None,
-                https_port='443',
-                https_private_key=None,
-                use_scgi=False,
-                scgi_host='127.0.0.1',
-                scgi_port=4000):
+                host='127.0.0.1', port='8080'):
     host = host or None
     import twisted.web2.wsgi
     import twisted.web2.log
-    import twisted.web2.http
+    import twisted.web2.channel
     import twisted.web2.server
-    import twisted.application.service
-    import twisted.application.strports
+    import twisted.internet.reactor
     wsgi_resource = twisted.web2.wsgi.WSGIResource(wsgi_app)
-    res = twisted.web2.log.LogWrapperResource(wsgi_resource)
+    resource = twisted.web2.log.LogWrapperResource(wsgi_resource)
     twisted.web2.log.DefaultCommonAccessLoggingObserver().start()
-    site = twisted.web2.server.Site(res)
-    # @@: I don't get this:
-    application = twisted.application.service.Application("paster")
-    if asbool(use_http):
-        s = twisted.application.strports.service(
-            conn_string(host, port),
-            twisted.web2.http.HTTPFactory(site))
-        s.setServiceParent(application)
-    if asbool(use_https):
-        s = twisted.application.strports.service(
-            conn_string(https_host, https_port,
-                        privateKey=https_private_key),
-            twisted.web2.http.HTTPFactory(site))
-        s.setServiceParent(application)
-    if asbool(use_scgi):
-        import twisted.web2.scgichannel
-        s = twisted.application.strports.service(
-            conn_string(scgi_host, scgi_port),
-            twisted.web2.scgichannel.SCGIFactory(site))
-        s.setServiceParent(application)
-    # @@: Now how the heck do I start it?
+    site = twisted.web2.server.Site(resource)
+    factory = twisted.web2.channel.HTTPFactory(site)
+    # --- start reactor for listen port
+    twisted.internet.reactor.listenTCP(int(port), factory, interface=host)
+    twisted.internet.reactor.run()
