699a9e57d6722c7a8592f96a0ea772cddaf681e2
ianb <ianb@localhost>
2007-01-09 22:13:24 +0000

    Improve egg_info_dir function

--- paste/script/pluginlib.py
+++ paste/script/pluginlib.py
@@ -118,4 +118,13 @@ def egg_name(dist_name):
     return pkg_resources.to_filename(pkg_resources.safe_name(dist_name))
 
 def egg_info_dir(base_dir, dist_name):
-    return os.path.join(base_dir, egg_name(dist_name) + '.egg-info')
+    first = None
+    for dir_extension in ['.'] + os.listdir(base_dir):
+        full = os.path.join(base_dir, dir_extension,
+                            egg_name(dist_name)+'.egg-info')
+        if first is not None:
+            first = full
+        if os.path.exists(full):
+            return full
+    raise IOError("No egg-info directory found (looked in %s)"
+                  % first)
