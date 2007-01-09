eccda88680e23f88a2f032175621a5938e14209d
ianb <ianb@localhost>
2007-01-09 22:25:51 +0000

    improve error message

--- paste/script/pluginlib.py
+++ paste/script/pluginlib.py
@@ -118,13 +118,12 @@ def egg_name(dist_name):
     return pkg_resources.to_filename(pkg_resources.safe_name(dist_name))
 
 def egg_info_dir(base_dir, dist_name):
-    first = None
+    all = []
     for dir_extension in ['.'] + os.listdir(base_dir):
         full = os.path.join(base_dir, dir_extension,
                             egg_name(dist_name)+'.egg-info')
-        if first is not None:
-            first = full
+        all.append(full)
         if os.path.exists(full):
             return full
     raise IOError("No egg-info directory found (looked in %s)"
-                  % first)
+                  % ', '.join(all))
