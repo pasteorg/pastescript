2d5e155b2e41c20fcd9e7c9bec115752876ac8b4
ianb <ianb@localhost>
2007-03-11 20:42:08 +0000

    Patch from POX for Cheetah import

--- paste/script/appinstall.py
+++ paste/script/appinstall.py
@@ -529,9 +529,8 @@ class Installer(object):
                 print 'No %s found' % meta_name
             return self.simple_config(vars)
         if self.use_cheetah:
-            if Cheetah is None:
-                import Cheetah.Template
-            tmpl = Cheetah.Template(self.dist.get_metadata(meta_name),
+            from Cheetah.Template import Template
+            tmpl = Template(self.dist.get_metadata(meta_name),
                                     searchList=[vars])
             return copydir.careful_sub(
                 tmpl, vars, meta_name)
