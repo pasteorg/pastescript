788446d1b174a6e228e0b947b174b80cdd8bbc6f
ianb <ianb@localhost>
2007-12-17 17:40:33 +0000

    Added umask option to flup_fastcgi; deprecated flup entry points

--- paste/script/flup_server.py
+++ paste/script/flup_server.py
@@ -2,11 +2,20 @@
 # Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
 from paste.deploy.converters import aslist, asbool
 from paste.script.serve import ensure_port_cleanup
+import warnings
+
+def warn(name, stacklevel=3):
+    # Deprecated 2007-12-17
+    warnings.warn(
+        'The egg:PasteScript#flup_%s entry point is deprecated; please use egg:Flup#flup_%s instead'
+        % (name, name),
+        DeprecationWarning, stacklevel=stacklevel)
 
 def run_ajp_thread(wsgi_app, global_conf,
                    scriptName='', host='localhost', port='8009',
                    allowedServers='127.0.0.1'):
     import flup.server.ajp
+    warn('ajp_thread')
     addr = (host, int(port))
     ensure_port_cleanup([addr])
     s = flup.server.ajp.WSGIServer(
@@ -21,6 +30,7 @@ def run_ajp_fork(wsgi_app, global_conf,
                  scriptName='', host='localhost', port='8009',
                  allowedServers='127.0.0.1'):
     import flup.server.ajp_fork
+    warn('ajp_fork')
     addr = (host, int(port))
     ensure_port_cleanup([addr])
     s = flup.server.ajp_fork.WSGIServer(
@@ -33,9 +43,10 @@ def run_ajp_fork(wsgi_app, global_conf,
 
 def run_fcgi_thread(wsgi_app, global_conf,
                     host=None, port=None,
-                    socket=None,
+                    socket=None, umask=None,
                     multiplexed=False):
     import flup.server.fcgi
+    warn('fcgi_thread')
     if socket:
         assert host is None and port is None
         sock = socket
@@ -45,17 +56,20 @@ def run_fcgi_thread(wsgi_app, global_conf,
         ensure_port_cleanup([sock])
     else:
         sock = None
+    if umask is not None:
+        umask = int(umask)
     s = flup.server.fcgi.WSGIServer(
         wsgi_app,
-        bindAddress=sock,
+        bindAddress=sock, umask=umask,
         multiplexed=asbool(multiplexed))
     s.run()
 
 def run_fcgi_fork(wsgi_app, global_conf,
                   host=None, port=None,
-                  socket=None,
+                  socket=None, umask=None,
                   multiplexed=False):
     import flup.server.fcgi_fork
+    warn('fcgi_fork')
     if socket:
         assert host is None and port is None
         sock = socket
@@ -65,9 +79,11 @@ def run_fcgi_fork(wsgi_app, global_conf,
         ensure_port_cleanup([sock])
     else:
         sock = None
+    if umask is not None:
+        umask = int(umask)
     s = flup.server.fcgi_fork.WSGIServer(
         wsgi_app,
-        bindAddress=sock,
+        bindAddress=sock, umask=umask,
         multiplexed=asbool(multiplexed))
     s.run()
 
@@ -75,6 +91,7 @@ def run_scgi_thread(wsgi_app, global_conf,
                     scriptName='', host='localhost', port='4000',
                     allowedServers='127.0.0.1'):
     import flup.server.scgi
+    warn('scgi_thread')
     addr = (host, int(port))
     ensure_port_cleanup([addr])
     s = flup.server.scgi.WSGIServer(
@@ -89,6 +106,7 @@ def run_scgi_fork(wsgi_app, global_conf,
                   scriptName='', host='localhost', port='4000',
                   allowedServers='127.0.0.1'):
     import flup.server.scgi_fork
+    warn('scgi_fork')
     addr = (host, int(port))
     ensure_port_cleanup([addr])
     s = flup.server.scgi_fork.WSGIServer(
