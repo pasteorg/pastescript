f7908bc2ef0d2936dc15bf5ef642f5df7a953b34
pjenvey <pjenvey@localhost>
2009-08-04 02:26:27 +0000

    include special here and __file__ defaults for fileConfig, similar to PasteDeploy configs

--- paste/script/command.py
+++ paste/script/command.py
@@ -769,11 +769,16 @@ class Command(object):
         """
         Setup logging via the logging module's fileConfig function with the
         specified ``config_file``, if applicable.
+
+        ConfigParser defaults are specified for the special ``__file__``
+        and ``here`` variables, similar to PasteDeploy config loading.
         """
         parser = ConfigParser.ConfigParser()
         parser.read([config_file])
         if parser.has_section('loggers'):
-            fileConfig(config_file)
+            config_file = os.path.abspath(config_file)
+            fileConfig(config_file, dict(__file__=config_file,
+                                         here=os.path.dirname(config_file)))
 
 class NotFoundCommand(Command):
 
