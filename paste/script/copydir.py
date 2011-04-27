71d3070702c73070e8abaf90590b7c00584f8690
Ian Bicking <ianb@colorstudy.com>
2011-04-27 13:11:08 -0500

    fix % formatting of skip message

--- paste/script/copydir.py
+++ paste/script/copydir.py
@@ -185,7 +185,7 @@ def should_skip_file(name):
     if name.endswith('~') or name.endswith('.bak'):
         return 'Skipping backup file %(filename)s'
     if name.endswith('.pyc') or name.endswith('.pyo'):
-        return 'Skipping %s file %(filename)s' % os.path.splitext(name)[1]
+        return 'Skipping %s file %%(filename)s' % os.path.splitext(name)[1]
     if name.endswith('$py.class'):
         return 'Skipping $py.class file %(filename)s'
     if name in ('CVS', '_darcs'):
