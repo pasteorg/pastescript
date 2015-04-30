a33de498fb838aa11960b67c957c8cdfac7ef763
Victor Stinner <victor.stinner@gmail.com>
2015-05-01 00:32:12 +0200

    Add README.rst: created from the long_description of setup.py

--- setup.py
+++ setup.py
@@ -32,25 +32,8 @@ if os.path.exists(news):
 if not found_news:
     print('Warning: no news for this version found')
 
-long_description="""\
-This is a pluggable command-line tool.
-
-It includes some built-in features;
-
-* Create file layouts for packages.  For instance, ``paste create
-  --template=basic_package MyPackage`` will create a `setuptools
-  <http://peak.telecommunity.com/DevCenter/setuptools>`_-ready
-  file layout.
-
-* Serving up web applications, with configuration based on
-  `paste.deploy <http://pythonpaste.org/deploy/paste-deploy.html>`_.
-
-The latest version is available in a `Mercurial repository
-<http://bitbucket.org/ianb/pastescript/>`_.
-
-For the latest changes see the `news file
-<http://pythonpaste.org/script/news.html>`_.
-"""
+with open("README.rst") as fp:
+    long_description = fp.read().strip()
 
 if found_news:
     title = 'Changes in %s' % version
