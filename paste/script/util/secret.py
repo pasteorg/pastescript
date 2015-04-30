970437948981fd47ecaed6e3bf078e527cd34a17
Victor Stinner <victor.stinner@gmail.com>
2015-05-01 00:42:47 +0200

    Port paste.script.util.secret to Python 3
    Add unit tests (test_util.py)

--- paste/script/util/secret.py
+++ paste/script/util/secret.py
@@ -4,8 +4,10 @@
 Create random secrets.
 """
 
+import base64
 import os
 import random
+import six
 from six.moves import range
 
 def random_bytes(length):
@@ -16,8 +18,8 @@ def random_bytes(length):
     try:
         return os.urandom(length)
     except AttributeError:
-        return ''.join([
-            chr(random.randrange(256)) for i in range(length)])
+        return b''.join([
+            six.int2byte(random.randrange(256)) for i in range(length)])
 
 def secret_string(length=25):
     """
@@ -25,7 +27,10 @@ def secret_string(length=25):
     is a base64-encoded version of a set of random bytes, truncated
     to the given length (and without any newlines).
     """
-    s = random_bytes(length).encode('base64')
+    s = random_bytes(length)
+    s = base64.b64encode(s)
+    if six.PY3:
+        s = s.decode('ascii')
     for badchar in '\n\r=':
         s = s.replace(badchar, '')
     # We're wasting some characters here.  But random characters are
