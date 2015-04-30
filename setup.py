d62bcab85b23fc9e4dbefa22662517bcf638b798
Victor Stinner <victor.stinner@gmail.com>
2015-05-01 00:36:07 +0200

    Set version to 2.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@
 from setuptools import setup, find_packages
 import re, os, sys
 
-version = '1.7.5'
+version = '2.0'
 
 news = os.path.join(os.path.dirname(__file__), 'docs', 'news.txt')
 found_news = ''
