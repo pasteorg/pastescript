0226915e1da9a6e9be4dc359c1ada6cfdbcb636f
Victor Stinner <victor.stinner@gmail.com>
2015-04-30 23:04:21 +0200

    Skip tests/test_template_introspect.py if Cheetah module is missing
    This change is needed to run tests on Python 3.

--- tests/test_template_introspect.py
+++ tests/test_template_introspect.py
@@ -1,13 +1,18 @@
+from nose import SkipTest
 import os
 from paste.script import templates
 
 tmpl_dir = os.path.join(os.path.dirname(__file__), 'sample_templates')
 
 def test_find():
+    try:
+        import Cheetah.Template
+    except ImportError:
+        raise SkipTest("need Cheetah.Template")
+
     vars = templates.find_args_in_dir(tmpl_dir, True)
     assert 'a' in vars
     assert vars['a'].default is templates.NoDefault
     assert 'b' in vars
     assert vars['b'].default == 1
     assert len(vars) == 2
-    
