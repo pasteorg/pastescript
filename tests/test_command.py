71af815700a362645302fab473c3c731c0846e21
Victor Stinner <victor.stinner@gmail.com>
2015-05-26 10:23:42 +0200

    Fix test_help()

--- tests/test_command.py
+++ tests/test_command.py
@@ -1,12 +1,47 @@
 #!/usr/bin/env python
 from paste.script import command
 from paste.script import create_distro
+import six
+import sys
+import textwrap
 import unittest
 
 
 class TestCommand(unittest.TestCase):
-    def test_help(self):
-        command.run(['--help'])
+   def test_help(self):
+        usage = textwrap.dedent('''
+            Usage: test_command.py [paster_options] COMMAND [command_options]
+
+            Options:
+              --version         show program's version number and exit
+              --plugin=PLUGINS  Add a plugin to the list of commands (plugins are Egg
+                                specs; will also require() the Egg)
+              -h, --help        Show this help message
+
+            Commands:
+              create       Create the file layout for a Python distribution
+              grep         Search project for symbol
+              help         Display help
+              make-config  Install a package and create a fresh config file/directory
+              points       Show information about entry points
+              post         Run a request for the described application
+              request      Run a request for the described application
+              serve        Serve the described application
+              setup-app    Setup an application, given a config file
+        ''').strip() + "\n\n"
+
+        stdout = sys.stdout
+        try:
+            sys.stdout = six.StringIO()
+            try:
+                command.run(['--help'])
+            except SystemExit as exc:
+                self.assertEqual(exc.code, 0)
+            else:
+                self.fail("SystemExit not raised")
+            self.assertEqual(usage, sys.stdout.getvalue())
+        finally:
+            sys.stdout = stdout
 
 
 class TestCreateDistroCommand(unittest.TestCase):
@@ -16,7 +51,7 @@ class TestCreateDistroCommand(unittest.TestCase):
     def test_list_templates(self):
         self.cmd.run(['--list-templates'])
 
-    def test_create_distro(self):
+    def test_template(self):
         self.cmd.run(['--template=basic_package', 'test'])
 
 
