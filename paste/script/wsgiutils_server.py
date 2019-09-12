27163b724fb6345fddc8ced32d03aea58ada6583
Alexander <shasha@shasha-zenbook-pro-14.home>
2019-09-12 14:29:24 +0200

    Replacing WSGIUtils with WSGIServer for python3 compatibility

--- paste/script/wsgiutils_server.py
+++ paste/script/wsgiutils_server.py
@@ -5,14 +5,15 @@ from paste.translogger import TransLogger
 
 def run_server(wsgi_app, global_conf, host='localhost',
                port=8080):
-    from wsgiutils import wsgiServer
+    import wsgiserver
+
     logged_app = TransLogger(wsgi_app)
     port = int(port)
     # For some reason this is problematic on this server:
     ensure_port_cleanup([(host, port)], maxtries=2, sleeptime=0.5)
     app_map = {'': logged_app}
-    server = wsgiServer.WSGIServer((host, port), app_map)
+    server = wsgiserver.WSGIServer(logged_app, host=host, port=port)
     logged_app.logger.info('Starting HTTP server on http://%s:%s',
                            host, port)
-    server.serve_forever()
+    server.start()
 
